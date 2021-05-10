d89df569a3f76f179873b0a26006c46f4a2a9167
Jacob Kucera <kucerajacob@gmail.com>
2021-05-10 00:48:13 -0700

    typing feature

--- script.js
+++ script.js
@@ -135,3 +135,100 @@ function smoothScroll(target) {
 		scrollTop: target.offset().top - 58
 	}, 600);
 }
+
+// asdkfljhasdkfjhaklsjdfhalksdjhfkaljsdhfalskjfhakjlsdfh
+
+init = () => {
+    wait(250).then(() => {
+        clearText();
+        typeText(" ").then(typeLoop);
+    });
+
+    typeLoop = () => {
+        typeText("HI, I'M JACOB. \n")
+			.then(() => wait(800))
+			.then(() => typeText("A UI/UX DESIGNER."))
+            // .then(() => removeText("HI, I'M JACOB. \n A UI/UX DESIGNER."))
+            // .then(typeLoop);
+    }
+}
+
+const elementNode = document.getElementById("landing-text");
+let text = "";
+
+updateNode = () => {
+    elementNode.innerText = text;
+}
+
+pushCharacter = (character) => {
+    text += character;
+    updateNode();
+}
+
+popCharacter = () => {
+    text = text.slice(0, text.length - 1);
+    updateNode();
+}
+
+clearText = () => {
+    text = "";
+    updateNode();
+}
+
+wait = (time) => {
+    if (time === undefined) {
+        const randomMsInterval = 100 * Math.random();
+        time = randomMsInterval < 50 ? 10 : randomMsInterval;
+    }
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            requestAnimationFrame(resolve);
+        }, time);
+    });
+}
+
+typeCharacter = (character) => {
+    return new Promise((resolve) => {
+        pushCharacter(character);
+        wait().then(resolve);
+    });
+}
+
+removeCharacter = () => {
+    return new Promise((resolve) => {
+        popCharacter();
+        wait().then(resolve);
+    });
+};
+
+typeText = (text) => {
+    return new Promise((resolve) => {
+        function type([character, ...text]) {
+            typeCharacter(character).then(() => {
+                if (text.length) type(text);
+                else resolve();
+            });
+        }
+
+        type(text);
+    });
+}
+
+removeText = ({ length: amount }) => {
+    return new Promise((resolve) => {
+        function remove(count) {
+            removeCharacter().then(() => {
+                if (count > 1) {
+                    remove(count - 1);
+                } else {
+                    resolve();
+                }
+            });
+        }
+
+        remove(amount);
+    });
+}
+
+init();
